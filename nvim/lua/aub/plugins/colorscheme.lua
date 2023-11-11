eac8e01842005a41d068df58f70b1a9e1ba0f246
ayoubissaad <ayoub.issaad@gmail.com>
2023-11-11 14:37:03 +0100

    added tokyonight

--- nvim/lua/aub/plugins/colorscheme.lua
+++ nvim/lua/aub/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
 	{
-		"bluz71/vim-nightfly-guicolors",
+		-- "bluz71/vim-nightfly-guicolors",
+		"folke/tokyonight.nvim",
+		lazy = false,
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme nightfly]])
+			vim.cmd([[colorscheme tokyonight-night]])
 		end,
 	},
 }
